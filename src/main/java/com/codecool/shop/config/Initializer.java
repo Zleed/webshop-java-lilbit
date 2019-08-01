20d5d01ff9b395cf434871db1aad0259ff5b64ac
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-08-02 01:28:24 +0200

    Products and Pics are added

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -30,10 +30,12 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(lenovo);
         Supplier google = new Supplier("Google", "Internet-related services and products");
         supplierDataStore.add(google);
-        Supplier onePlus = new Supplier("OnePlus","OnePlus, is a Chinese smartphone manufacturer based in Shenzhen, Guangdong.");
+        Supplier onePlus = new Supplier("OnePlus", "OnePlus, is a Chinese smartphone manufacturer based in Shenzhen, Guangdong.");
         supplierDataStore.add(onePlus);
         Supplier asus = new Supplier("Asus", "AsusTek Computer Inc. is a Taiwan-based multinational computer and phone hardware and electronics company");
         supplierDataStore.add(asus);
+        Supplier dell = new Supplier("Dell", "Dell is a US multinational computer technology company that develops, sells, repairs, and supports computers and related products and services.");
+        supplierDataStore.add(dell);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
@@ -47,7 +49,6 @@ public class Initializer implements ServletContextListener {
         //setting up products and printing it
         // Tablets
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Amsazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 
@@ -55,6 +56,10 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Google Pixel 3", 733.07f, "USD", "Even into 2019, the Google Pixel 3 has the best smartphone camera you can buy.", phone, google));
         productDataStore.add(new Product("One Plus 7 Pro", 853.89f, "USD", "The One Plus 7 Pro delivers an exceptional Android experience by combining premium hardware and software.", phone, onePlus));
         productDataStore.add(new Product("Asus ROG Phone", 899.f, "USD", "It will no doubt be one of the most expensive gaming phones on the market.", phone, asus));
+        productDataStore.add(new Product("Google Pixel 2", 233, "USD", "The Google Pixel 2 clues us in on why Google called its phone series Pixel: it was building the best camera on a phone.", phone, google));
 
+        //Notebooks
+
+        productDataStore.add(new Product("Dell XPS 15", 3151.29f, "USD", "Premium high-performance laptop with the latest 8th generation IntelCor i9, huge memory, 1TB SSD and a virtually borderless InfinityEdge UHD 4K display.", noteBook, dell));
     }
 }
