fa27899bc11c4e6b2acde9864550d4ad622a9d5b
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-08-15 00:19:51 +0200

    BCrypt algorithm added to util package

--- src/main/java/com/codecool/shop/controller/LoginController.java
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.OrderDao;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.implementation.OrderDaoMem;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.model.Order;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,27 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/"})
-public class CategoryController extends HttpServlet {
+@WebServlet(urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        context.setVariable("categories", productCategoryDataStore.getAll());
-
-        engine.process("product/categories.html", context, resp.getWriter());
+        engine.process("product/login.html", context, resp.getWriter());
     }
 
 //    @Override
 //    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        System.out.println(req.getAttribute("orderID"));
-//        OrderDao orderDataStore = OrderDaoMem.getInstance();
-//        Order order = orderDataStore.find(1);
-//        System.out.println(order.getStatus());
 //        doGet(req, resp);
 //    }
 }
---
+++ src/main/java/com/codecool/shop/controller/SignInController.java
@@ -0,0 +1,35 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.util.BCrypt;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/signin"})
+public class SignInController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/signin.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String hashed = BCrypt.hashpw(req.getParameter("password"), BCrypt.gensalt());
+
+        resp.sendRedirect("/");
+    }
+}
